996e177b1fe9d4e43dfbece54feec20c2259629d
Abdullah Al Emon <77507222+abdullahalemon2@users.noreply.github.com>
2021-01-16 05:32:46 +0600

    Update and rename termux-key.py to auto-P2.py

--- auto-P2.py
+++ auto-P2.py
@@ -1,14 +1,14 @@
 import os
 from time import sleep
 
-#by Zheck Savalas
+#by Emon Vaw
 
 a ='\033[92m'
 b ='\033[91m'
 c ='\033[0m'
 os.system('clear')
 print(a+'\t  Shorcut for help you')
-print(b+'\t    Zheck Savalas')
+print(b+'\t    Emon Vaw')
 print('\t http://bit.ly/S4vLs')
 print(a+'+'*40)
 print('\nProses..')
@@ -34,8 +34,6 @@ sleep(1)
 print(b+'\n[!] Setting up..')
 sleep(2)
 os.system('termux-reload-settings')
-print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi ZheckSavalas untuk requests\natau pertanyaan, atau hubungi http://bit.ly/S4vLs\nThanks :*\n\n')
+print(a+'[!] Setup Success !!')
 
 
-# ini cuma shortcut buat bantu para nub
-# Zheck Savalas
